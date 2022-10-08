2c14a8ad1ac46d1cc43687bede149d0073d54a2d
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-09 05:23:58 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+// sum of two integers
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first number = 10;
+    int second number = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first number+ second number;
+    System.out.println("The sum is: " + sum);
+  }
+}
