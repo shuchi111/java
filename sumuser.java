7fa6995d2d505be125e01817cca5f07a963ed654
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-09 06:17:18 +0530

    Update and rename sum_user_input.java to sumuser.java

--- sumuser.java
+++ sumuser.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  // Import the Scanner class
 
-public class MyClass {
+public class sumuser{
   public static void main(String[] args) {
     int x, y, sum;
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
