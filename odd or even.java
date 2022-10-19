4f7948bd81ad444505a3cf2f9a844ac522787399
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-20 02:56:20 +0530

    check number is odd or even

---
+++ odd or even.java
@@ -0,0 +1,26 @@
+//Check whether a number is even or odd using if...else statement
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
+
+
+/*Output
+
+Enter a number: 12
+12 is even
+*/
